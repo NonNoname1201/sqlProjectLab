b051832a3a489d5533fc7b4b94145ccfb30f87cd
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-11 15:35:57 +0100

    Added TableMarki.sql

---
+++ TableMarki.sql
@@ -0,0 +1,11 @@
+create table if not exists Marki (
+    /*
+    Marki (Brands):
+    BrandID (INT, PRIMARY KEY)
+    Name (VARCHAR)
+    Country (VARCHAR)
+    */
+    id int primary key auto_increment not null unique,
+    name varchar(255) not null unique,
+    country varchar(255) not null
+)
