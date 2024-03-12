28e5b5de26683eefaa4be6d0015bd52bebee8895
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-12 17:33:53 +0100

    Code standardization, cleanup

--- TableProdukty.sql
+++ TableProdukty.sql
@@ -1,25 +1,26 @@
+drop table if exists Produkty;
 create table if not exists Produkty
 (
     /*
     Produkty (Products):
-    ProductID (INT, PRIMARY KEY)
-    Name (VARCHAR)
-    BrandID (INT, FOREIGN KEY REFERENCES Brands(BrandID))
+    ProductID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+    Name (VARCHAR, NOT NULL)
+    BrandID (INT, FOREIGN KEY REFERENCES Brands(BrandID), NOT NULL)
     CategoryID (INT, FOREIGN KEY REFERENCES Categories(CategoryID))
-    Description (TEXT)
-    Price (DECIMAL)
-    StockQuantity (INT)
-    Warranty (INT)
+    Description (TEXT, NOT NULL)
+    Price (DECIMAL, NOT NULL)
+    StockQuantity (INT, NOT NULL)
+    Warranty (VARCHAR)
     */
 
     id             int primary key auto_increment not null unique,
     name           varchar(30)                    not null,
     brand_id       int                            not null,
     foreign key (brand_id) references marki (id),
-    category_id    int                            not null,
+    category_id    int,
     foreign key (category_id) references kategorie (id),
-    description    text,
+    description    text                           not null,
     price          decimal(10, 2)                 not null,
     stock_quantity int                            not null,
-    warranty       int                            not null
+    warranty       varchar(300)                   not null
 )
