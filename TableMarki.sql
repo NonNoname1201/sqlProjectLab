7bf9fa692f6328b2a1049afa97bcf068b37e6cd1
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-12 14:20:13 +0100

    Formatted code in TableMarki.sql

--- TableMarki.sql
+++ TableMarki.sql
@@ -1,11 +1,12 @@
-create table if not exists Marki (
+create table if not exists Marki
+(
     /*
     Marki (Brands):
     BrandID (INT, PRIMARY KEY)
     Name (VARCHAR)
     Country (VARCHAR)
     */
-    id int primary key auto_increment not null unique,
-    name varchar(255) not null unique,
-    country varchar(255) not null
+    id      int primary key auto_increment not null unique,
+    name    varchar(255)                   not null unique,
+    country varchar(255)                   not null
 )
