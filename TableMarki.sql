7670ef59836b6392e00d6b9fe48fce27e9ec5560
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-12 17:32:18 +0100

    Rework on brands, now each brand will refer to countries table

--- TableMarki.sql
+++ TableMarki.sql
@@ -1,12 +1,15 @@
+drop table if exists Marki;
 create table if not exists Marki
 (
     /*
     Marki (Brands):
-    BrandID (INT, PRIMARY KEY)
-    Name (VARCHAR)
-    Country (VARCHAR)
+    BrandID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+    Name (VARCHAR, NOT NULL, UNIQUE)
+    CountryID (INT, FOREIGN KEY REFERENCES Countries(CountryID), NOT NULL)
     */
+
     id      int primary key auto_increment not null unique,
-    name    varchar(255)                   not null unique,
-    country varchar(255)                   not null
+    name    varchar(30)                    not null unique,
+    country int                            not null,
+    foreign key (country) references kraje (id)
 )
