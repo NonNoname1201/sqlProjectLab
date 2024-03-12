81a04fd3bd8f50200c5492730837ab51c8002788
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2024-03-12 17:31:34 +0100

    Reworked User-Client-Employee dependencies

--- TablePracownicy.sql
+++ TablePracownicy.sql
@@ -1,23 +1,24 @@
-create table if not exists Pracownicy
+drop table if exists Pracownicy;
+create table Pracownicy
 (
     /*
     Pracownicy (Employees):
-    EmployeeID (INT, PRIMARY KEY)
-    FirstName (VARCHAR)
-    LastName (VARCHAR)
-    Position (VARCHAR)
-    Email (VARCHAR)
-    Phone (VARCHAR)
-    HireDate (DATE)
-    Salary (DECIMAL)
+    EmployeeID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+    FirstName (VARCHAR, NOT NULL)
+    LastName (VARCHAR, NOT NULL)
+    Position (VARCHAR, NOT NULL)
+    Email (VARCHAR, NOT NULL, UNIQUE)
+    Phone (VARCHAR, NOT NULL, UNIQUE)
+    HireDate (DATE, NOT NULL)
+    HourlyRate (DECIMAL, NOT NULL)
     */
 
-    id         int primary key auto_increment not null unique,
-    first_name VARCHAR(30)                    not null,
-    fast_name  VARCHAR(30)                    not null,
-    position   VARCHAR(30)                    not null,
-    email      VARCHAR(100)                   not null,
-    phone      VARCHAR(13)                    not null,
-    hire_date  DATE                           not null,
-    salary     DECIMAL                        not null
+    id          int primary key auto_increment not null unique,
+    first_name  VARCHAR(30)                    not null,
+    fast_name   VARCHAR(30)                    not null,
+    position    VARCHAR(30)                    not null,
+    email       VARCHAR(100)                   not null unique,
+    phone       VARCHAR(13)                    not null unique,
+    hire_date   DATE                           not null,
+    hourly_rate DECIMAL                        not null
 );
