20fa50663f2f8c70804938ff0c214d72020048c8
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-03-20 13:33:03 +0100

    Rework on tables now total 22

--- script.sql
+++ script.sql
@@ -1,45 +1,63 @@
 /*
-1. Klienci (Customers):
-CustomerID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
-FirstName (VARCHAR, NOT NULL)
-LastName (VARCHAR, NOT NULL)
-Email (VARCHAR, NOT NULL, UNIQUE)
-Phone (VARCHAR, NOT NULL, UNIQUE)
-Address (VARCHAR, NOT NULL)
-City (VARCHAR, NOT NULL)
 
-2. Pracownicy (Employees):
+Countries (Countries):
+CountryID (INT, PRIMARY KEY, NOT NULL)
+Name (VARCHAR, NOT NULL, UNIQUE)
+
+Cities (Cities):
+CityID (INT, PRIMARY KEY, NOT NULL)
+Name (VARCHAR, NOT NULL, UNIQUE)
+CountryID (INT, FOREIGN KEY REFERENCES Countries(CountryID), NOT NULL)
+
+Address (Address):
+AddressID (INT PRIMARY KEY NOT NULL)
+PostalCode (VARCHAR(20) NOT NULL)
+Street (VARCHAR(255) NOT NULL)
+CityID (INT, FOREIGN KEY REFERENCES Cities(CityID), NOT NULL)
+State (VARCHAR(100))
+
+Positions (Positions):
+PositionID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+Name (VARCHAR, NOT NULL, UNIQUE)
+Description (TEXT, NOT NULL)
+
+Employees (Employees):
 EmployeeID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 FirstName (VARCHAR, NOT NULL)
 LastName (VARCHAR, NOT NULL)
-Position (VARCHAR, NOT NULL)
+PositionID (INT, FOREIGN KEY REFERENCES Positions(PositionID), NOT NULL)
 Email (VARCHAR, NOT NULL, UNIQUE)
 Phone (VARCHAR, NOT NULL, UNIQUE)
 HireDate (DATE, NOT NULL)
 HourlyRate (DECIMAL, NOT NULL)
 
-3. Sklepy (Stores):
+Customers (Customers):
+CustomerID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+FirstName (VARCHAR, NOT NULL)
+LastName (VARCHAR, NOT NULL)
+Email (VARCHAR, UNIQUE)
+Phone (VARCHAR, NOT NULL, UNIQUE)
+AddressID (INT, FOREIGN KEY REFERENCES Address(AddressID), NOT NULL)
+
+Stores (Stores):
 StoreID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 Name (VARCHAR, NOT NULL)
-Address (VARCHAR, NOT NULL, UNIQUE)
-City (VARCHAR, NOT NULL)
-Country (VARCHAR, NOT NULL)
+AddressID (INT, FOREIGN KEY REFERENCES Address(AddressID), NOT NULL)
+StoreManagerID (INT, FOREIGN KEY REFERENCES Employees(EmployeeID), NOT NULL)
+State (enum('open', 'closed', 'under construction'), NOT NULL)
 
-4. Kategorie (Categories):
+Categories (Categories):
 CategoryID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 Name (VARCHAR, NOT NULL, UNIQUE)
 ParentCategoryID (INT, FOREIGN KEY REFERENCES Categories(CategoryID))
 
-5. Kraje (Countries):
-CountryID (INT, PRIMARY KEY, NOT NULL)
-Name (VARCHAR, NOT NULL, UNIQUE)
-
-6. Marki (Brands):
+Brands (Brands):
 BrandID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 Name (VARCHAR, NOT NULL, UNIQUE)
-CountryID (INT, FOREIGN KEY REFERENCES Countries(CountryID), NOT NULL)
+Description (TEXT, NOT NULL)
+AddressID (INT, FOREIGN KEY REFERENCES Address(AddressID), NOT NULL)
 
-7. Produkty (Products):
+Products (Products):
 ProductID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 Name (VARCHAR, NOT NULL)
 BrandID (INT, FOREIGN KEY REFERENCES Brands(BrandID), NOT NULL)
@@ -47,86 +65,83 @@ CategoryID (INT, FOREIGN KEY REFERENCES Categories(CategoryID))
 Description (TEXT, NOT NULL)
 Price (DECIMAL, NOT NULL)
 StockQuantity (INT, NOT NULL)
-Warranty (VARCHAR)
 
-8. Użytkownicy (Users):
+Users (Users):
 UserID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
-CustomerID (INT, FOREIGN KEY REFERENCES Customers(CustomerID), UNIQUE)
-EmployeeID (INT, FOREIGN KEY REFERENCES Employees(EmployeeID), UNIQUE)
 Username (VARCHAR, NOT NULL)
 Password (VARCHAR, NOT NULL)
+Email (VARCHAR, NOT NULL, UNIQUE)
+Phone (VARCHAR, UNIQUE)
+AddressID (INT, FOREIGN KEY REFERENCES Address(AddressID), NOT NULL)
 Role (ENUM('admin', 'user'), NOT NULL)
 
-9. Koszyk (Cart):
+Cart (Cart):
 CartID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
-CustomerID (INT, FOREIGN KEY REFERENCES Customers(CustomerID), NOT NULL, UNIQUE)]
-TotalQuantity (INT, NOT NULL)
+UserID (INT, FOREIGN KEY REFERENCES Users(UserID), NOT NULL)
 
-10. Produkt w koszyku (OrderProducts):
+CartProduct (CartProduct):
 CartProductID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
-CartID (INT, FOREIGN KEY REFERENCES Orders(OrderID), NOT NULL)
+CartID (INT, FOREIGN KEY REFERENCES Cart(CartID), NOT NULL)
 ProductID (INT, FOREIGN KEY REFERENCES Products(ProductID), NOT NULL)
 Quantity (INT, NOT NULL)
-Price (DECIMAL, NOT NULL)
 
-11. Zamówienia (Orders):
+Orders (Orders):
 OrderID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
-CustomerID (INT, FOREIGN KEY REFERENCES Customers(CustomerID), NOT NULL)
-StoreID (INT, FOREIGN KEY REFERENCES Stores(StoreID), NOT NULL)
+UserID (INT, FOREIGN KEY REFERENCES Users(UserID), NOT NULL)
 OrderDate (DATETIME, NOT NULL)
 ShipDate (DATETIME)
 Status (ENUM('pending', 'shipping', 'delivered'), NOT NULL)
 TotalAmount (DECIMAL, NOT NULL)
 
-12. Produkt w zamówieniu (OrderProducts):
+OrderProducts (OrderProducts):
 OrderProductID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 OrderID (INT, FOREIGN KEY REFERENCES Orders(OrderID), NOT NULL)
 ProductID (INT, FOREIGN KEY REFERENCES Products(ProductID), NOT NULL)
 Quantity (INT, NOT NULL)
-Price (DECIMAL, NOT NULL)
 
-13. Metody płatności (PaymentMethods):
+PaymentMethods (PaymentMethods):
 PaymentMethodID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 Name (VARCHAR, NOT NULL, UNIQUE)
 Description (TEXT, NOT NULL)
 
-14. Transakcje (Transactions):
+Transactions (Transactions):
 TransactionID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
-OrderID (INT, FOREIGN KEY REFERENCES Orders(OrderID), NOT NULL, UNIQUE)
+OrderID (INT, FOREIGN KEY REFERENCES Orders(OrderID), UNIQUE)
 PaymentMethodID (INT, FOREIGN KEY REFERENCES PaymentMethods(PaymentMethodID), NOT NULL)
 Amount (DECIMAL, NOT NULL)
 TransactionDate (DATETIME, NOT NULL)
 
--- Promocje (Promotions):
--- PromotionID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
--- Name (VARCHAR, NOT NULL)
--- Description (TEXT, NOT NULL)
--- DiscountPercent (DECIMAL, NOT NULL)
--- StartDate (DATE, NOT NULL)
--- EndDate (DATE)
-
-15. Punkty lojalnościowe (LoyaltyPoints):
-LoyaltyPointID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+Checkouts (Checkouts):
+CheckoutID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 CustomerID (INT, FOREIGN KEY REFERENCES Customers(CustomerID), NOT NULL)
+TotalAmount (DECIMAL, NOT NULL)
+CheckoutDate (DATETIME, NOT NULL)
+
+CheckoutProducts (CheckoutProducts):
+CheckoutProductID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+CheckoutID (INT, FOREIGN KEY REFERENCES Checkouts(CheckoutID), NOT NULL)
+ProductID (INT, FOREIGN KEY REFERENCES Products(ProductID), NOT NULL)
+Quantity (INT, NOT NULL)
+
+Promotions (Promotions):
+PromotionID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+Name (VARCHAR, NOT NULL)
+Description (TEXT, NOT NULL)
+StartDate (DATE, NOT NULL)
+EndDate (DATE)
+
+LoyaltyPoints (LoyaltyPoints):
+LoyaltyPointID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
+UserID (INT, FOREIGN KEY REFERENCES Users(UserID), NOT NULL)
 Points (INT, NOT NULL)
 ExpiryDate (DATE, NOT NULL)
 
-16. Ocena produktu (ProductRating):
+ProductRating (ProductRating):
 ProductRatingID (INT, PRIMARY KEY, NOT NULL, UNIQUE)
 ProductID (INT, FOREIGN KEY REFERENCES Products(ProductID), NOT NULL)
-ClientID (INT,  FOREIGN KEY REFERENCES Customers(CustomerID), NOT NULL)
+UserID (INT, FOREIGN KEY REFERENCES Users(UserID), NOT NULL)
 Rating (DECIMAL)
 Description (TEXT, NOT NULL)
 
-17. Adres (Address):
-AddressID (INT PRIMARY KEY NOT NULL)
-ClientID (INT FOREIGN KEY REFERENCES Customers(CustomerID) NOT NULL)
-Street (VARCHAR(255) NOT NULL)
-City (VARCHAR(100) NOT NULL)
-State (VARCHAR(100) NOT NULL)
-PostalCode (VARCHAR(20) NOT NULL)
-Country (VARCHAR(100) NOT NULL)
-
-
-Total - 17 tabel
+Total - 22 tables
 */
