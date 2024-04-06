3deee678a2dd35d76c311f1a00043b0d869337f7
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-04-06 15:35:50 +0200

    Addition of records to TableOrderProducts.sql

--- TableOrderProducts.sql
+++ TableOrderProducts.sql
@@ -14,4 +14,12 @@ create table order_products
     product_id int                            not null,
     foreign key (product_id) references products (id),
     quantity   int                            not null
-)
+);
+
+insert into order_products (order_id, product_id, quantity)
+values (1, 1, 1),
+         (1, 2, 2),
+         (2, 1, 3),
+         (2, 2, 4),
+         (3, 1, 5),
+         (3, 2, 6);
